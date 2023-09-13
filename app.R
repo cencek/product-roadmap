fcb8d69b80ba968877369df140a77da78e3ba73b
cencek <87336335+cencek@users.noreply.github.com>
2023-09-13 11:17:55 -0400

    Updating further
    Adding function to split by design and build phases, and adding release date variable

--- app.R
+++ app.R
@@ -1,19 +1,21 @@
 # Load necessary libraries
 library(shiny)
 library(ggplot2)
+library(plotly)
 
 # Sample data
 sample_data <- data.frame(
   product_name = c("Project A", "Project B", "Project C"),
   start_date = as.Date(c("2023-01-01", "2023-02-01", "2023-03-01")),
   end_date = as.Date(c("2023-01-15", "2023-02-28", "2023-04-15")),
+  design_start = as.Date(c("2022-12-01", "2023-01-15", "2023-02-20")),
+  design_end = as.Date(c("2023-01-01", "2023-02-01", "2023-03-01")),
+  release_date = as.Date(c("2023-01-20", "2023-03-05", "2023-04-18")),
   project_type = c("Feature add", "New release", "Technical debt")
 )
 
-# Add numeric version of product_name and mid_date for plotting
+sample_data$release_text <- paste("Release date:", sample_data$release_date)
 sample_data$numeric_product <- as.numeric(as.factor(sample_data$product_name))
-sample_data$mid_date <- sample_data$start_date + 
-  (sample_data$end_date - sample_data$start_date)/2
 
 # UI for the Shiny app
 ui <- fluidPage(
@@ -24,28 +26,31 @@ ui <- fluidPage(
                   choices = c("All", unique(sample_data$project_type)))
     ),
     mainPanel(
-      plotOutput("timelinePlot")
+      plotlyOutput("timelinePlot")
     )
   )
 )
 
 # Server logic for the Shiny app
 server <- function(input, output) {
-  output$timelinePlot <- renderPlot({
+  output$timelinePlot <- renderPlotly({
     filtered_data <- sample_data
     if (input$selected_type != "All") {
       filtered_data <- sample_data[sample_data$project_type == input$selected_type, ]
     }
     
-    ggplot(filtered_data, aes(x = numeric_product, ymin = start_date, ymax = end_date)) +
-      geom_rect(aes(xmin = numeric_product - 0.4, 
-                    xmax = numeric_product + 0.4, fill = project_type)) +
+    p <- ggplot(filtered_data, aes(x = numeric_product, fill = project_type, text = product_name)) +
+      geom_rect(aes(ymin = design_start, ymax = design_end, xmin = numeric_product - 0.4, xmax = numeric_product + 0.4), alpha = 0.3) +
+      geom_rect(aes(ymin = start_date, ymax = end_date, xmin = numeric_product - 0.4, xmax = numeric_product + 0.4)) +
+      geom_point(aes(y = release_date, text = release_text), color = "black") +
       scale_x_continuous(breaks = filtered_data$numeric_product, 
                          labels = filtered_data$product_name) +
       coord_flip() +
       labs(title = "Product Roadmap Timeline", x = "", y = "Date") +
       theme_minimal() +
-      scale_fill_manual(values = c("Feature add" = "blue", "New release" = "darkgreen", "Technical debt" = "purple"))
+      scale_fill_manual(values = c("Feature add" = "blue", "New release" = "green", "Technical debt" = "red"))
+    
+    ggplotly(p, tooltip = "text")
   })
 }
 
